a27235345f053b393d259bc4498ee3dabdbf7a53
ponkotuy <web@ponkotuy.com>
2022-08-06 10:10:09 +0900

    Remove print

--- lib/exif_parser.dart
+++ lib/exif_parser.dart
@@ -6,7 +6,6 @@ import 'package:exif/exif.dart';
 class ExifParser {
   static Future<String?> parseCreateDate(File file) async {
     final tags = await readExifFromBytes(file.readAsBytesSync());
-    print(tags['EXIF DateTimeOriginal']?.printable);
     return tags['EXIF DateTimeOriginal']?.printable;
   }
 }
