463a4dc9b98ce106a3ba0afd77f7245190b053b8
ponkotuy <web@ponkotuy.com>
2022-08-13 00:54:12 +0900

    Fix #5

--- lib/image.dart
+++ lib/image.dart
@@ -2,6 +2,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 
 class ImageWidget extends StatelessWidget {
   const ImageWidget({Key? key, required this.file}) : super(key: key);
@@ -11,12 +12,17 @@ class ImageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if(file == null) {
-      return Text(
-        'None', style: Theme.of(context).textTheme.headline4,
-      );
+      return Text('None', style: Theme.of(context).textTheme.headline4,);
     }
+    final fname = basename(file!.path);
     return Expanded(
-        child: Image(image: FileImage(file!),)
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(child: Image(image: FileImage(file!), fit: BoxFit.contain,)),
+          Text(fname, style: Theme.of(context).textTheme.caption),
+        ],
+      )
     );
   }
 }
